13de9a56989747973b15b1a350aa4bca203cffe5
lalithadsuresh <lalithsuresh2004@gmail.com>
2023-05-22 16:23:54 -0600

    answered

--- userconstructor.py
+++ userconstructor.py
@@ -1,1 +1,28 @@
-hello.
+class User:
+
+    def __init__(self, username, password):
+        self.likes = 0
+        self.username = username
+        self.password = password
+
+    def display_info(self):
+        ("username:", self.username)
+        ("password:", self.password)
+
+class SignUp:
+
+    def __init__(self):
+        self.users = []
+
+    def registered_users(self, username, password):
+        new_user = User(username, password)
+        self.users.append(new_user)
+        print("user registered")
+
+    def displayed_users(self):
+        size = len(self.users)
+        print(size, "registered users:")
+        for user in self.users:
+            user.display_info()
+        
+    
